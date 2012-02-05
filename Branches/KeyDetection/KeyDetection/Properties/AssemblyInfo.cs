1f045753cdecf76440d38e81305bf78b6268c4b8
raphaelchang <raphaelchang@1c69c455-704f-4920-a974-72747f0ffa95>
2012-02-05 11:14:32 +0000

    Fixed Sobel edge detection algorithm and threshold detection to properly detect the edge of the key.
    -Raphael

--- Branches/KeyDetection/KeyDetection/Properties/AssemblyInfo.cs
+++ Branches/KeyDetection/KeyDetection/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeyDetection")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeyDetection")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("761d4a66-db3c-473f-8834-aa0ff052e318")]
+[assembly: Guid("afa344c8-e8c3-4a3a-9c0d-05879c442932")]
 
 // Version information for an assembly consists of the following four values:
 //
