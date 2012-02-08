1fd9318283bb0452080b05100ccfe3f9d2d935bc
tonypeng <tonypeng@1c69c455-704f-4920-a974-72747f0ffa95>
2012-02-08 04:23:20 +0000

    Added missing files from the last commit... -Tony

---
+++ Trunk/KeyDetection/KeyDetection/Options.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyDetection
+{
+    public partial class Options : Form
+    {
+        public Options()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Globals.PPI_RATIO = float.Parse(textBox1.Text);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid content...");
+            }
+        }
+    }
+}
