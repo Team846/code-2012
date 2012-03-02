08a7a0a74e1b98889856c9d1bccc7dfc80d32eec
rbtying <rbtying@1c69c455-704f-4920-a974-72747f0ffa95>
2012-03-02 06:28:36 +0000

    Added explicit exposure setting

--- Trunk/KeyTracker++/ubuntu_eclipse/KeyTracker/beagleboard/start.sh
+++ Trunk/KeyTracker++/ubuntu_eclipse/KeyTracker/beagleboard/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-CAMERA=1
+CAMERA=0
 
 # enable white balance auto
 # disable exposure auto
 # disable focus auto
-v4l2-ctl -d "/dev/video$CAMERA" -c exposure_auto=1 -c focus_auto=0 -c white_balance_temperature_auto=1
+v4l2-ctl -d "/dev/video$CAMERA" -c exposure_auto=1 -c focus_auto=0 -c white_balance_temperature_auto=1 -c exposure_absolute=156
 
 KEYTRACKER=/home/ubuntu/KeyTracker/beagleboard/KeyTracker
 #KEYTRACKER=./KeyTracker_gui
