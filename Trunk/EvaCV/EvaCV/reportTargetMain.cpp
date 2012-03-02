c7165fea59744c40b753e55cb72bfe86084125b2
raphaelchang <raphaelchang@1c69c455-704f-4920-a974-72747f0ffa95>
2012-03-02 05:12:44 +0000

    Updated packet structure

--- Trunk/EvaCV/EvaCV/reportTargetMain.cpp
+++ Trunk/EvaCV/EvaCV/reportTargetMain.cpp
@@ -15,7 +15,6 @@ using namespace std;
 //
 static int thresh = 200;
 static int saveN = 0;
-static bool sendConstantly = false;
 //
 
 void
@@ -29,6 +28,7 @@ main (int argc  , char ** argv  )
 {
   FindTarget target;
   ReportSlop reportSlop;
+  int count = 0;
 
   if (!target.tg_ready ())
     {
@@ -36,50 +36,29 @@ main (int argc  , char ** argv  )
       return -1;
     }
 
-  //target.tg_printProperties ();
+  target.tg_printProperties ();
 
   if (argc > 1)
     {
        thresh = atoi(argv[1]);
     }
-  if (argc > 2)
-    {
-       sendConstantly = true;
-    }
   printf ("==== threshold %i ====\n", thresh);
 
   signal (SIGUSR1, onSignal);
 
-
-    clock_t start, end;
-    int avgFrameCount = 30;
-
-int countt = 0;
   for (;;)
     {
-//if (countt++ >= 10) exit(0);
-	++countt;
-
-        if (countt % avgFrameCount == 1) {
-            start = clock();
-        }
-
+	  ++count;
       target.tg_getFrame ();	// get a new frame from camera
       // printf ("==== %i ====\n", countt++);
 
       target.tg_findSquares (thresh);
       target.tg_getTarget ();
-      //printf ("slop = %i, top = %i, rowTarget = %i sendConstantly = %i\n", target.tg_getSlop (), target.tg_getTop (), target.tg_getRowTarget (), sendConstantly);
-	if ((target.tg_getSlop() != 0xfff) || sendConstantly) {
-	      reportSlop.sendResults (target.tg_getSlop (), target.tg_getTop ());
-	}
-      //DDDtarget.tg_saveImageIfAsked (saveN);
+      printf ("slop = %i, top = %i, rowTarget = %i\n", target.tg_getSlop (),
+	      target.tg_getTop (), target.tg_getRowTarget ());
+      reportSlop.sendResults (target.tg_getSlop (), target.tg_getTop (), count);
+      target.tg_saveImageIfAsked (saveN);
 //      waitKey (1);		//waits for a key: it also handles the GUI events.
-        if (countt % avgFrameCount == 0) {
-            end = clock();
-            double t = CLOCKS_PER_SEC * avgFrameCount * 1.0 / (end - start);
-            printf( "rtFPS: %.02f\n",t);
-        }
     }
   return 0;
 }
--- Trunk/EvaCV/EvaCV/startRT.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/EvaCV
-./reportTarget 200 
