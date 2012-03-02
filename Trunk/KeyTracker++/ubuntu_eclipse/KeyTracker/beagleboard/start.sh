e7a78c2a981fc00da88b157d4eed3b8fdda6ef87
rbtying <rbtying@1c69c455-704f-4920-a974-72747f0ffa95>
2012-03-02 02:08:47 +0000

    V4L2 start script update

--- Trunk/KeyTracker++/ubuntu_eclipse/KeyTracker/beagleboard/start.sh
+++ Trunk/KeyTracker++/ubuntu_eclipse/KeyTracker/beagleboard/start.sh
@@ -6,7 +6,7 @@ CAMERA=1
 # disable focus auto
 v4l2-ctl -d "/dev/video$CAMERA" -c exposure_auto=1 -c focus_auto=0 -c white_balance_temperature_auto=1
 
-#KEYTRACKER=/home/ubuntu/KeyTracker/beagleboard/KeyTracker
-KEYTRACKER=./KeyTracker_gui
+KEYTRACKER=/home/ubuntu/KeyTracker/beagleboard/KeyTracker
+#KEYTRACKER=./KeyTracker_gui
 
 $KEYTRACKER $CAMERA
