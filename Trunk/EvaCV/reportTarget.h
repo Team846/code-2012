c0a6be2b444d1d53c884754d88ba47e5b0c81d49
evalomazov <evalomazov@1c69c455-704f-4920-a974-72747f0ffa95>
2012-03-03 23:37:19 +0000

    hardcoded camera # to 6

---
+++ Trunk/EvaCV/reportTarget.h
@@ -0,0 +1,75 @@
+#ifndef FIND_TARGET_H
+#define FIND_TARGET_H
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+
+//
+class ReportSlop
+{
+public:
+  int sendResults (int slop, bool top, int packetID);
+    ReportSlop ();
+   ~ReportSlop ();
+private:
+  void rs_init ();
+  void rs_reset ();
+  int m_soc;
+  struct sockaddr_in m_dst;
+};
+
+//
+class FindTarget
+{
+
+public:
+  FindTarget ();
+  ~FindTarget ();
+
+  void tg_getFrame ();
+  bool tg_ready ();
+  void tg_saveImageIfAsked (int saveN);
+  void tg_getTarget ();
+  void tg_findSquares (int thresh);
+  void tg_drawSquares ();
+  void tg_printProperties ();
+
+  inline int tg_getSlop ()
+  {
+    return slop;
+  };
+  inline int tg_getRowTarget ()
+  {
+    return rowTarget;
+  };
+  inline int tg_getTop ()
+  {
+    return top;
+  };
+private:
+  VideoCapture cap;
+  vector < vector < Point > >squares;
+  Mat image;
+  Mat blackWhite;
+  int slop;
+  int rowTarget;
+  bool top;
+  double tg_angle (Point & pt1, Point & pt2, Point & pt0);
+
+};
+
+#endif	// FIND_TARGET_H
---
+++ Trunk/EvaCV/reportTargetMain.cpp
@@ -0,0 +1,91 @@
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+#include <signal.h>
+
+#include "reportTarget.h"
+
+//
+using namespace cv;
+using namespace std;
+//
+static int thresh = 200;
+static int saveN = 0;
+static bool sendConstantly = false;
+//
+
+void
+onSignal (int a)
+{				//signal 10 handler. produces number used in save picture filename
+  saveN++;
+}
+
+int
+main (int argc  , char ** argv  )
+{
+  FindTarget target;
+  ReportSlop reportSlop;
+
+  if (!target.tg_ready ())
+    {
+      printf ("camera is not accessible\n");
+      return -1;
+    }
+
+  //target.tg_printProperties ();
+
+  if (argc > 1)
+    {
+       thresh = atoi(argv[1]);
+    }
+  if (argc > 2)
+    {
+       sendConstantly = true;
+    }
+  printf ("==== threshold %i ====\n", thresh);
+
+  signal (SIGUSR1, onSignal);
+
+
+    clock_t start, end;
+    int avgFrameCount = 30;
+
+int countt = 0;
+
+  int nextPacketID = 1;
+
+  for (;;)
+    {
+//if (countt++ >= 10) exit(0);
+	++countt;
+
+        if (countt % avgFrameCount == 1) {
+            start = clock();
+        }
+
+      target.tg_getFrame ();	// get a new frame from camera
+      // printf ("==== %i ====\n", countt++);
+
+      target.tg_findSquares (thresh);
+      target.tg_getTarget ();
+      //printf ("slop = %i, top = %i, rowTarget = %i sendConstantly = %i\n", target.tg_getSlop (), target.tg_getTop (), target.tg_getRowTarget (), sendConstantly);
+	if ((target.tg_getSlop() != 0xfff) || sendConstantly) {
+	      reportSlop.sendResults (target.tg_getSlop (), target.tg_getTop (), nextPacketID);
+	      printf("---- %i --- %i \n",target.tg_getSlop (), target.tg_getTop ());
+	      
+              ++nextPacketID;
+        }
+      //DDDtarget.tg_saveImageIfAsked (saveN);
+//      waitKey (1);		//waits for a key: it also handles the GUI events.
+        if (countt % avgFrameCount == 0) {
+            end = clock();
+            double t = CLOCKS_PER_SEC * avgFrameCount * 1.0 / (end - start);
+            printf( "rtFPS: %.02f\n",t);
+        }
+    }
+  return 0;
+}
