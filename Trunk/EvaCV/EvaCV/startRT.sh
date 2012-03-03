00313bbae30a78315dcea8419557fbd7d8bb971b
dmitrylomazov <dmitrylomazov@1c69c455-704f-4920-a974-72747f0ffa95>
2012-03-03 01:30:13 +0000

    Calibrated exposure setting

--- Trunk/EvaCV/EvaCV/startRT.sh
+++ Trunk/EvaCV/EvaCV/startRT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CAMERA=6
 
-v4l2-ctl -d "/dev/video$CAMERA" -c exposure_auto=1 -c focus_auto=1 -c white_balance_temperature_auto=1 -c exposure_absolute=156
+v4l2-ctl -d "/dev/video$CAMERA" -c exposure_auto=1 -c focus_auto=1 -c white_balance_temperature_auto=1 -c exposure_absolute=3
 
 cd /home/ubuntu/EvaCV
 ./reportTarget 200 $CAMERA
