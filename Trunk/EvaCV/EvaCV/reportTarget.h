593a920387501fcf8a0f414dbd433bfddc1f5601
evalomazov <evalomazov@1c69c455-704f-4920-a974-72747f0ffa95>
2012-03-10 22:18:44 +0000

    Documented the h file for reportTarget. -Eva

--- Trunk/EvaCV/EvaCV/reportTarget.h
+++ Trunk/EvaCV/EvaCV/reportTarget.h
@@ -1,6 +1,10 @@
-#ifndef FIND_TARGET_H
-#define FIND_TARGET_H
-//
+/** h file for ReportTarget.
+ *
+ *  This program finds the targets (shaped like rectangles) in the frames provided
+ *  by the camera. It then states the location of the square closest to the center in
+ *  terms of the x-values. Lastly it figures out whether the square is on the top or 
+ *  the middle.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,71 +20,65 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+/** \namespace  cv: openCV namespace
+ */
 using namespace cv;
 
-//
+/** \class ReportSlop
+ *  \brief Sends packets with the slop in it.
+ */
 class ReportSlop
 {
-public:
-  int sendResults (int slop, bool top, int packetID);
-    ReportSlop ();
-   ~ReportSlop ();
-private:
-  void rs_init ();
-  void rs_reset ();
-  int m_soc;
-  struct sockaddr_in m_dst;
+  public:
+    int sendResults (int slop, bool top);
+       ReportSlop ();
+      ~ReportSlop ();
+
+  private:
+    void rs_init ();
+    void rs_reset ();
+    int m_soc;
+    struct sockaddr_in m_dst;
 };
 
-//
+/** \class FindTarget
+ *  \brief Finds the target closest to the center.
+ * 
+ *  The class returns the distance from the target to the center on the x-axis, and
+ *  whether the target is the top or middle one.
+ */
 class FindTarget
 {
+  public:
+     FindTarget ();
+    ~FindTarget ();
+    void tg_getFrame ();                             //Get frame from camera.
+    bool tg_ready ();                                //Check that camera is ready.
+    void tg_saveImageIfAsked (int saveN);            //Save frame to disk.
+    void tg_getTarget ();                            //Find the target.
+    void tg_findSquares (int thresh, bool showWin);  //Find the squares in the image.
+    void tg_drawSquares ();                          //Outline the squares.
+    void tg_printProperties ();                      //Prints all camera parameters.
+    inline int tg_getSlop ()        //Finds distance between target and center.
+    {
+      return slop;
+    };
+    inline int tg_getRowTarget ()    
+    {
+      return rowTarget;
+    };
+    inline int tg_getTop ()         //Finds whether target is top or middle one.
+    {
+      return top;
+    };
 
-public:
-  FindTarget (int camN);
-  ~FindTarget ();
-
-  void tg_getFrame ();
-  bool tg_ready ();
-  void tg_saveImageIfAsked (int saveN);
-  void tg_getTarget ();
-  void tg_findSquares (int thresh);
-  void tg_drawSquares ();
-  void tg_printProperties ();
-
-  inline int tg_getSlop ()
-  {
-    return slop;
-  };
-  inline int tg_getRowTarget ()
-  {
-    return rowTarget;
-  };
-  inline int tg_getTop ()
-  {
-    return top;
-  };
-  inline void tg_setVertThresh (int vThresh)
-  {
-    vertThresh = vThresh;
-  };
-  inline void tg_setBrightThresh (int bThresh)
-  {
-    brightThresh = bThresh;
-  };
-private:
-  VideoCapture cap;
-  vector < vector < Point > >squares;
-  Mat image;
-  Mat blackWhite;
-  int slop;
-  int rowTarget;
-  bool top;
-  int vertThresh;
-  int brightThresh;
-  double tg_angle (Point & pt1, Point & pt2, Point & pt0);
-  FindTarget ();
-
+  private:
+    VideoCapture cap;
+    vector < vector < Point > >squares;
+    Mat image;
+    Mat blackWhite; 
+    int slop;         //Distance between target and center of screen.
+    int rowTarget;        
+    bool top;         //Top or middle target.   
+    double tg_angle (Point & pt1, Point & pt2, Point & pt0);  //Finds right angles.
 };
-
-#endif	// FIND_TARGET_H
